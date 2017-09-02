a87c66b4c53ee6c7500938e89dd7d507f565cc0c
Gabriel Singer <gsinger@ucdavis.edu>
2017-09-02 06:11:40 -0800

    Finishing off the instructions.

--- Github_Instructions.R
+++ Github_Instructions.R
@@ -4,4 +4,12 @@
 # 2. save
 # 3. click git dropdown, select commit
 # 4. select files to commit to "new-branch" repo, add comments to designate changes
-# 5. 
+# 5. CLose commit window
+# 6. Open git shell in more drop down
+# 7. Type in "git push origin new-branch", assuming new-branch is the name of you branched repo
+# 8. go to github online and make a new pull request, merging data from new-branch into the master repo JSATS_Filtering....
+# 9. merge new-branch and master
+
+# Figure out if I have a local copy of new branch
+# I don't think that there is a local copy of the branch it is just a temporary branch to write and edit code without
+# fucking everything up.  You can delete the branch each time after the merge is completed.
